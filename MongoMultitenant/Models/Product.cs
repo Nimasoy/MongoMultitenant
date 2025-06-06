f1e573d154f0b3f5a10059eb07d1cee58df1e25d
Nimasoy <nimarezaei.me@gmail.com>
2025-06-06 15:31:00 +0330

    Add project files.

---
+++ MongoMultitenant/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace MongoMultitenant.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+        public string ?ProductName { get; set; }
+        public string ?Description { get; set; }
+    }
+}
